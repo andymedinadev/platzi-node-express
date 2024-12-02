886bed2d4880b5ba92b0653bd08480f9a46f069c
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-12-02 12:01:41 -0300

    chore: test first routes

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 const express = require('express')
+const { faker } = require('@faker-js/faker')
 
 const app = express()
 const port = 3000
+
+app.get('/products', (req, res) => {
+  const products = []
+
+  const { size } = req.query
+  const limit = size || 5
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.urlPicsumPhotos()
+    })
+  }
+  res.json(products)
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('Test')
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  res.json({
+    id,
+    name: 'Product2',
+    price: 200
+  })
+})
+
+app.get('/products/test', (req, res) => {
+  res.send('No funciona')
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json({
+    category: categoryId,
+    product: productId
+  })
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+
+  if (limit && offset) {
+    res.json({
+      limit, offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Server en express')

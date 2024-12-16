3c972c2432062c9904b9b8a664958d28ab7bc876
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-12-16 08:34:35 -0300

    feat: add cors middleware

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const routerApi = require('./routes')
+const cors = require('cors')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+const whiteList = ['http://127.0.0.1:5500', 'http://localhost:8080', 'http://localhost:3000']
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('origen no permitido'))
+    }
+  }
+}
+
+app.use(cors(options))
 
 app.get('/', (req, res) => {
   res.send('Server en express')
